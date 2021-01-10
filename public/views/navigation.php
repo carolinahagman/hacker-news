1c115e553f54328b85ba251e2f4fdf24861a9c51
Carolina <hagmancarolina@gmail.com>
2021-01-10 21:48:34 +0100

    Update styling on nav

--- public/views/navigation.php
+++ public/views/navigation.php
@@ -1,6 +1,6 @@
-<nav class="w-screen p-3">
+<nav class="w-screen p-3 flex justify-center">
 
-	<ul class="w-full flex justify-around dark:text-gray-600 ">
+	<ul class="w-full max-w-2xl flex justify-evenly dark:text-gray-600 ">
 		<li class="">
 			<a class="text-yellow-600 uppercase font-bold text-xl" href="/index.php"><?= $config['title']; ?></a>
 		</li><!-- /nav-item -->
